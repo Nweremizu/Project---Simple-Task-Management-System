d862544810c3f40836700dd5be1cc988df50a5bd
Nweremizu <nweremizu.bruno@stu.cu.edu.ng>
2024-01-19 18:17:09 +0100

    edit server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 const PORT = 3000;
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 // Connect to my MongoDB Database
