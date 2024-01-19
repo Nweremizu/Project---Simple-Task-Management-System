c3fcd3755fd575b291c19018e1811f482c849360
Nweremizu <nweremizu.bruno@stu.cu.edu.ng>
2024-01-19 17:24:08 +0100

    first commit

---
+++ public/script.js
@@ -0,0 +1,136 @@
+// Server Functions
+
+const fetch_todos = async () => {
+	const res = await fetch("http://localhost:3000/todos");
+	const data = await res.json();
+	let number_of_tasks = data.length;
+	let number_of_completed_tasks = data.filter((todo) => todo.completed).length;
+
+	const todo_count = document.querySelector(".total");
+	todo_count.innerHTML = number_of_tasks;
+	const completed_count = document.querySelector(".completed");
+	completed_count.innerHTML = `${number_of_completed_tasks} of ${number_of_tasks}`;
+
+	const todo_list = document.querySelector(".todo-list");
+	todo_list.innerHTML = "";
+	data.forEach((todo) => {
+		const todo_item = document.createElement("div");
+		let id = todo._id.toString();
+		todo_item.classList.add("task");
+		todo_item.setAttribute("data-id", todo._id);
+		todo_item.innerHTML = `
+			<input type="checkbox" data-id=${todo._id}>
+			<p class="main-stuff">${todo.task}</p>
+			<button class='delete-btn' onclick=delete_task('${id}')><img src="images/delete.svg" alt="delete"></button>
+		`;
+
+		if (todo.completed) {
+			todo_item.classList.add("completed-task");
+			todo_item.querySelector("input").checked = true;
+		}
+
+		todo_list.appendChild(todo_item);
+	});
+};
+
+fetch_todos();
+
+const add_todo = async (task) => {
+	await fetch("http://localhost:3000/todos", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ task, completed: false }),
+	});
+
+	fetch_todos();
+};
+
+const delete_todo = async (id) => {
+	await fetch(`http://localhost:3000/todos`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ id }),
+	});
+
+	fetch_todos();
+};
+
+const update_todo = async (id, completed) => {
+	await fetch(`http://localhost:3000/todos/completed/`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ id, completed }),
+	});
+
+	fetch_todos();
+};
+
+const update_task = async (id, task) => {
+	await fetch("http://localhost:3000/todos/", {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ id, task }),
+	});
+
+	fetch_todos();
+};
+
+const add_task_input = document.querySelector("#add_task");
+const submit_btn = document.querySelector(".submit-btn");
+
+submit_btn.addEventListener("click", function (e) {
+	e.preventDefault();
+	if (add_task_input.value === "") {
+		alert("Add A Task!!");
+	} else {
+		add_todo(add_task_input.value);
+		add_task_input.value = "";
+	}
+});
+
+const todo_list = document.querySelector(".todo-list");
+function delete_task(id) {
+	delete_todo(id);
+}
+
+todo_list.addEventListener("click", function (e) {
+	if (e.target.tagName === "INPUT") {
+		let id = e.target.dataset.id;
+		let completed = e.target.checked;
+		update_todo(id, completed);
+	}
+});
+
+const modal_container = document.getElementById("modalContainer");
+
+function close_modal() {
+	modal_container.style.display = "none";
+}
+
+const close_btn = document.getElementById("closeModalBtn");
+close_btn.addEventListener("click", close_modal);
+
+todo_list.addEventListener("click", function (e) {
+	if (e.target.className === "task") {
+		modal_container.style.display = "flex";
+		document.querySelector("#edit_task").value =
+			e.target.querySelector(".main-stuff").innerHTML;
+		document.querySelector(".submit-btn-m").setAttribute("data-id", e.target.dataset.id);
+
+		document.querySelector(".submit-btn-m").addEventListener("click", function (x) {
+			x.preventDefault();
+			let task = document.querySelector("#edit_task").value;
+			let id = document.querySelector(".submit-btn-m").getAttribute("data-id");
+			update_task(id, task);
+			close_modal();
+		});
+	}
+});
---
+++ server.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+// Connect to my MongoDB Database
+mongoose.connect("mongodb://localhost:27017/todo");
+
+const todoSchema = new mongoose.Schema({
+	task: String,
+	completed: Boolean,
+});
+
+app.get("/", (req, res) => {
+	const indexPath = `${__dirname}/public/task_system.html`;
+	fs.readFile(indexPath, "utf8", (err, data) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send("Internal Server Error");
+		} else {
+			res.send(data);
+		}
+	});
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+//API Endpoints
+app.get("/todos", async (req, res) => {
+	const todos = await Todo.find();
+	res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+	const { task, completed } = req.body;
+	const newTodo = new Todo({ task, completed });
+	await newTodo.save();
+	res.json(newTodo);
+	console.log(newTodo);
+});
+
+app.delete("/todos/", async (req, res) => {
+	const { id } = req.body;
+	await Todo.findByIdAndDelete(id);
+	res.json({ message: "Todo Deleted" });
+	console.log(id);
+});
+
+// to update the completed status of a todo
+app.put("/todos/completed/", async (req, res) => {
+	const { id } = req.body;
+	const { completed } = req.body;
+	try {
+		const updatedTodo = await Todo.findByIdAndUpdate(id, { completed }, { new: true });
+		res.json(updatedTodo);
+	} catch (error) {
+		res.status(500).json({ error: "Error updating todo" });
+	}
+});
+
+app.put("/todos/", async (req, res) => {
+	const { id } = req.body;
+	const { task } = req.body;
+	try {
+		const update_Todo = await Todo.findByIdAndUpdate(id, { task }, { new: true });
+		res.json(update_Todo);
+	} catch (error) {
+		res.status(500).json({ error: "Error updating todo" });
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
